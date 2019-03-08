c3d61a6b5d018c34fe2dd0b949faff2929286b58
Joanna Yuhas <joannayuhas@gmail.com>
2019-03-08 18:40:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ function dec(n){
 }
 
 function makeInt(n){
-  return parseInt(n)
+  return parseInt(n,10)
 }
 
-function preserveDecimal (n)
+function preserveDecimal (n) {
+  return parseFloat(n)
+}
